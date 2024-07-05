0e5a20479702f6adeef17917669cd81d070a7f68
aniruddha <aniruddhathakur76@gmail.com>
2024-07-05 19:42:32 +0530

    Added whychooseUs and Testimonials cards Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import FeaturedCources from "@/components/ui/FeaturedCources";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCources />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
     </main>
   );
 }
